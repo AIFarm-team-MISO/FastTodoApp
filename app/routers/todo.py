695fc266b16c83d34c2617df20355ca499b2aff5
miso <sevenstar15@naver.com>
2024-10-06 18:07:19 +0900

    Define request and response models for API

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,16 +1,57 @@
 # Todo(할일) 관련 API
 
-from fastapi import APIRouter
+# 라우팅 , 예외처리
+from fastapi import APIRouter, HTTPException
+
+# API스키마
+from app.schemas import TodoCreate, TodoUpdate, TodoResponse
+from typing import List
+
 
 # 라우터 생성
 router = APIRouter()
 
-# todo 목록 가져오기 API
+# 임시 데이터베이스 역할을 하는 리스트
+todos_db = []
+
+# GET 요청: todo 목록 조회
 @router.get("/todos")
 async def get_tasks():
-    return {"todos": ["Task 1", "Task 2", "Task 3"]}
+    return todos_db
 
-# todo 추가 API
+'''
+    POST 요청: 새로운 할 일 생성
+    '/todos' 경로로 POST 요청이 들어오면 아래 함수가 실행됨
+    데이터베이스에 저장할 새로운 할일을 사용자가 요청시 보낸 제목, 본문, 완료값으로 딕셔너리로 생성
+    
+
+'''
 @router.post("/todos")
-async def create_tasks():
-    return {"message": "Todo created"}
+async def create_tasks(todo: TodoCreate):
+    new_todo = {
+        "id": len(todos_db) +1,
+        "title": todo.title,
+        "description":todo.description,
+        "is_completed":todo.is_competed
+    }
+    todos_db.append(new_todo)
+
+    return new_todo
+
+'''
+    PUT 요청: 기존의 할일을 찾아 수정
+    '/todos/{todo_id}' 경로로 PUT 요청이 들어오면 실행
+    해당 id의 할 일이 없는 경우 404 오류 반환
+
+'''
+@router.put("/todos/{todo_id}", response_model=TodoResponse)
+async def update_todo(todo_id: int, todo: TodoUpdate):
+    for existing_todo in todos_db:
+        if existing_todo["id"] == todo_id:
+            if todo.title is not None:
+                existing_todo["title"] = todo.title
+            if todo.description is not None:
+                existing_todo["description"]  = todo.description
+            existing_todo["is_completed"] = todo.is_competed
+            return existing_todo
+    raise HTTPException(status_code=404, detail="todo not found") 
--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,44 @@
-# Pydantic 스키마 정의 (요청/응답 데이터 검증)
+# Pydantic 스키마(모델) 정의 (요청/응답 데이터 검증)
+
+from pydantic import BaseModel
+from typing import Optional
+
+#
+'''
+    TodoCreate, TodoUpdate 등 공통필드들을 정의하는 모델
+    title은 필수이고, description과 is_completed는 선택적 필드
+
+'''
+class TodoBase(BaseModel):                  
+    title: str                               #할일의 제목(필수)
+    description: Optional[str] = None        #할일의본문 (선택)
+    is_competed: bool = False                #완료여부
+
+'''
+    할일 생성시 사용되는 모델
+    pass : 추가필드 없이 TodoBase 그대로 상속
+
+'''
+class TodoCreate(TodoBase):                  
+    pass
+
+'''
+    할일을 수정할 때 사용하는 모델
+    필요에 따라 title, description, is_completed를 업데이트 가능
+    title이 Optional로 설정되어 있어, 업데이트 시 제목은 선택사항
+'''
+class TodoUpdate(TodoBase):
+    title: Optional[str] = None  
+
+'''
+    API 응답 시 사용할 모델
+    데이터베이스에서 자동 생성되는 고유 ID를 포함하여 응답 데이터 구조를 정의
+    id : 할일의 고유 ID (데이터베이스에서 각 할 일을 식별하는 값, create 이후 DB에 저장이 되어서야 고유ID가 생성됨)
+    orm_mode(ORM 모드 설정) : SQLAlchemy 모델 객체에서 Pydantic 모델로 자동 변환을 가능하게 함
+'''
+class TodoResponse(TodoBase):
+    id: int                       
+
+    
+    class Config:
+        orm_mode = True
